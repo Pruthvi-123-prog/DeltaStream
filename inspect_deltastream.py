6288490d72897794f88db29363fd007874dec599
Pruthvi-123-prog <pruthvi123@example.com>
2026-04-27 17:56:52 +0530

    chore: remove internal development artifacts

--- inspect_deltastream.py
+++
@@ -1,7 +0,0 @@
-import deltastream, os
-print(deltastream.__file__)
-print(os.listdir(os.path.dirname(deltastream.__file__)))
-# inspect AutoModel
-from deltastream import AutoModel
-import inspect
-print(inspect.getsourcefile(AutoModel))
